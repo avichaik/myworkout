662eff13be8300bd91ddfc52df66977b025367f0
avichai kalij <avichai.kalij@gmail.com>
2019-03-22 17:28:59 +0200

    Updates

---
+++ precache-manifest.05517483c58eac1f4095eff9cf6d6125.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-workout/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "63d0da5a6e7abff43413",
+    "url": "/my-workout/static/js/runtime~main.d1bf28c3.js"
+  },
+  {
+    "revision": "c36878ba66d2d746d493",
+    "url": "/my-workout/static/js/main.351ce8ef.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/my-workout/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "c36878ba66d2d746d493",
+    "url": "/my-workout/static/css/main.7b38f319.chunk.css"
+  },
+  {
+    "revision": "b428bc15dc1e101c5103dbc18cbfd72c",
+    "url": "/my-workout/index.html"
+  }
+];
